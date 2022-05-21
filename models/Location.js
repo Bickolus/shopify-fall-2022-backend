a0d85981f61221f4da9386e68b518e3ba0e5084f
Bilal Raza <smbraza97@gmail.com>
2022-05-21 17:20:01 -0400

    added code for Product model and also added some comments for Location.js

--- models/Location.js
+++ models/Location.js
@@ -1,9 +1,12 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
 
-const sequelize = require('../config/connection.js');
-
+// Initialize Location model (table) by extending off Sequelize's Model class
 class Location extends Model {}
 
+// set up fields and rules for Location model
 Location.init(
   {
     id: {
