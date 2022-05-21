6c660990469242557a60492d3692d3dd1caa518e
Bilal Raza <smbraza97@gmail.com>
2022-05-21 19:41:57 -0400

    added REST routes for product and location

---
+++ routes/api/location-routes.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const { Product, Location } = require('../../models');
+
+// GET all locations
+router.get('/', async (req, res) => {
+  try {
+    const locationData = await Location.findAll({
+      // including its associated Products data
+      include: [{ model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'location_id'] }],
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single location by id
+router.get('/:id', async (req, res) => {
+  try {
+    const locationData = await Location.findByPk(req.params.id, {
+      // including its associated Products data
+      include: [{ model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'location_id'] }],
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No location/warehouse found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST (create) a new location
+router.post('/', async (req, res) => {
+  try {
+    const locationData = await Location.create(req.body);
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT (edit) a location using the location's id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedLocation = await Location.update(
+      {
+        location_name: req.body.location_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+    );
+
+    if (!updatedLocation) {
+      res.status(404).json({ message: 'No location/warehouse found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(updatedLocation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a location by its id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedLocation = await Location.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedLocation) {
+      res.status(404).json({ message: 'No location/warehouse found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(deletedLocation);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
---
+++ routes/api/product-routes.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const { Product, Location } = require('../../models');
+
+// GET all products
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      // including its associated Products data
+      include: [{ model: Location, attributes: ['id', 'location_name'] }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single product by id
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      // including its associated Products data
+      include: [{ model: Location, attributes: ['id', 'location_name'] }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST (create) a new product
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT (edit) a product using the product's id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedProduct = await Product.update(
+      {
+        product_name: req.body.product_name,
+        price: req.body.price,
+        stock: req.body.stock,
+        location_id: req.body.location_id,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      },
+    );
+
+    if (!updatedProduct) {
+      res.status(404).json({ message: 'No product found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE a product by its id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedProduct) {
+      res.status(404).json({ message: 'No product found with that ID!' });
+      return;
+    }
+
+    res.status(200).json(deletedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
