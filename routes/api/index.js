0e80dbcc2265972044dca6b9f46a64cac034069d
Bilal Raza <smbraza97@gmail.com>
2022-05-21 21:03:12 -0400

    finished writing routes in api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,4 +5,9 @@ const locationRoutes = require('./location-routes');
 router.use('/products', productRoutes);
 router.use('/locations', locationRoutes);
 
+// Default API route
+router.get('/', (req, res) => {
+    res.json({ message: 'Connection to Inventory API Successful!' });
+});
+
 module.exports = router;
