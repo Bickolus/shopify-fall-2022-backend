b9980ab456b7717ded2b9bf628f47ebdd1a0081a
Bilal Raza <smbraza97@gmail.com>
2022-05-21 21:02:54 -0400

    changes a couple things in model files

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@
 const Product = require('./Product');
 const Location = require('./Location');
 
+// For simplicity, we can make the assumption that a certain type of product is only found in one location at a time, which means that Products and Locations have a many-to-one relationship.
+
 // Products belongs to Location
 Product.belongsTo(Location, {
   foreign_key: 'location_id',
