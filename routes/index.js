3d1aee0f1c704f9bfd23183a99e95f020a8359ef
Bilal Raza <smbraza97@gmail.com>
2022-05-21 21:03:44 -0400

    added an example of a .env file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Incorrect route, please try something else!</h1>")
+  res.send("<h1>Hey, there is no front-end for this application, so try making some requests using the URL!</h1>")
 });
 
 module.exports = router;
